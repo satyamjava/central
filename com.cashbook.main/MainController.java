d7fbf5ee2494eb9e68cea6ef23fd423f16294169
Satyam Singh <satyam.zip@gmail.com>
2017-07-06 19:58:39 +1000

    initial commit

---
+++ com.cashbook.main/MainController.java
@@ -0,0 +1,21 @@
+package com.cashbook.main;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class MainController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainController.class, args);
+    }
+}
